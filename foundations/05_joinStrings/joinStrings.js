bebe3d02ea1dc0c4f5220ffdff410bd942c193d1
carolinacodeworksdev <bspringer.dev@gmail.com>
2026-05-10 20:23:45 -0400

    completed js exercise 05_joinStrings

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -9,9 +9,17 @@
 
 // Add your code right below, good luck!
 
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
 
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
 
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
+const greeting2 = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 // Do not change this
 module.exports = {
